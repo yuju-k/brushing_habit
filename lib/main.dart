8fb0767eeb471cd3c1b2532e00ec04720628acaf
yuju-k <tet31500@gmail.com>
2022-12-01 00:30:52 +0900

    홈메뉴

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Brushing_habit_DTx',
+      theme: ThemeData.dark(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('유아양치습관형성 DTx'),
@@ -18,11 +19,11 @@ class HomeScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                color: Colors.white,
+                color: Colors.black,
                 child: const HomeMenu(),
               ),
               Container(
-                color: Colors.white,
+                color: Colors.black,
                 child: const QuestionBrushing(),
               ),
             ],
@@ -35,13 +36,54 @@ class HomeScreen extends StatelessWidget {
 
 class HomeMenu extends StatelessWidget {
   const HomeMenu({Key? key}) : super(key: key);
-
+//메인페이지에 표기되는 메뉴화면 목록: 퍼즐보상모음, 영상보상모음, 달성표
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text('Menu Box')],
+        children: [
+          Container(
+            color: Colors.black,
+            child: const Text(
+              '메뉴',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black,
+            child: const Text(
+              '퍼즐보상모음',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black,
+            child: const Text(
+              '영상보상모음',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black,
+            child: const Text(
+              '달성표',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
