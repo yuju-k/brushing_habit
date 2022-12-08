d419a7b66a38aa54d6ac8813a414cb8389c60bda
yuju-k <tet31500@gmail.com>
2022-12-08 22:35:04 +0900

    아이폰 실행오류 문제 해결(MLKit 버전 문제였다)

---
+++ lib/screen/firebase_auth/firebase_auth.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  /* Sign In anon */
+  Future signInAnon() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User? user = result.user;
+      return _userFromFirebaseUser(user!);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // create user obj based on FirebaseUser
+  UserModel? _userFromFirebaseUser(User user) {
+    return user != null ? UserModel(uid: user.uid) : null;
+  }
+}
+
+class UserModel {
+  final String uid;
+
+  UserModel({required this.uid});
+}
